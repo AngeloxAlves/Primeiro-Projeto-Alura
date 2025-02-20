d79f66f6d2d9b9ff4a4698764885739585ce9bdf
Angelo Alves <angeloalves1580@gmail.com>
2025-02-20 11:19:51 -0300

    Deu Certo!

---
+++ app.js
@@ -0,0 +1,67 @@
+let listaNumeros = [];
+let qtdeMaxima = 4;
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = (texto);
+}
+
+function exibirMensagemInicial() {
+    exibirTextoNaTela('h1', 'Jogo do Número Secreto');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+}
+
+exibirMensagemInicial()
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+    let qtdeLista = listaNumeros.length;
+
+    if (qtdeLista == qtdeMaxima) {
+        listaNumeros = [];
+    }
+
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Acertou!');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}`;
+        exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('h1', 'Errou');
+            exibirTextoNaTela('p', 'O número secreto é menor');
+        } else {
+            exibirTextoNaTela('h1', 'Errou');
+            exibirTextoNaTela('p', 'O número secreto é maior');
+        }
+        tentativas++;
+        limparCampo();
+    }
+}
+
+function gerarNumeroAleatorio() {
+    let numeroSorteado = parseInt(Math.random() * 10 + 1);
+    if (listaNumeros.includes(numeroSorteado)) {
+        return gerarNumeroAleatorio();
+    } else {
+        listaNumeros.push(numeroSorteado);
+        console.log(listaNumeros);
+        return numeroSorteado;
+
+    }
+}
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciarJogo() {
+    tentativas = 1;
+    limparCampo();
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+    numeroSecreto = gerarNumeroAleatorio();
+}
